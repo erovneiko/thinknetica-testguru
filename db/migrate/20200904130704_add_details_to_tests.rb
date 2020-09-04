800ce202d3b14d057e589e26505017414f2ef360
Evgeni Rovneiko <erovneiko@mail.ru>
2020-09-04 16:26:39 +0300

    Значения по-умолчанию

--- db/migrate/20200904130704_add_details_to_tests.rb
+++ db/migrate/20200904130704_add_details_to_tests.rb
@@ -2,5 +2,6 @@ class AddDetailsToTests < ActiveRecord::Migration[6.0]
   def change
     change_column_null :tests, :title, true
     change_column_null :tests, :level, true
+    change_column_default :tests, :level, 0
   end
 end
--- db/migrate/20200904130715_add_answers_text_null_constraint.rb
+++ db/migrate/20200904130715_add_answers_text_null_constraint.rb
@@ -1,5 +1,6 @@
 class AddAnswersTextNullConstraint < ActiveRecord::Migration[6.0]
   def change
     change_column_null :answers, :text, true
+    change_column_default :answers, :correct, false
   end
 end
