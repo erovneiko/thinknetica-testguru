18bbc9edaa0defd3c397f8f8ae2a958e8f6b17df
Evgeni Rovneiko <erovneiko@mail.ru>
2020-09-08 09:47:00 +0300

    Доработки

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
   def get_tests(level)
     Test.where(level: level)
     .joins('JOIN user_tests ON user_tests.test_id = tests.id')
-    .joins('JOIN users ON users.id = user_tests.user_id')
     .where("user_tests.user_id = ?", id)
   end
 end
