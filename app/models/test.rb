f4d7c606318c1405e9273f22b08842eddca4aa57
Evgeni Rovneiko <erovneiko@mail.ru>
2020-09-15 15:47:24 +0300

    Скоупы и валидации

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,8 +4,19 @@ class Test < ApplicationRecord
   has_and_belongs_to_many :users
   belongs_to :author, class_name: "User"
 
-  def self.get_names(category)
-    Test.joins(:category).where('categories.title LIKE ?', category)
-    .order(title: :desc).pluck(:title)
-  end
+  scope :easy, -> { where(level: 0..1) }
+  scope :normal, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..) }
+  scope :categories_title, -> (category) { 
+    joins(:category)
+   .where('categories.title LIKE ?', category)
+   .order(title: :desc).pluck(:title)
+  }
+  scope :by_user_level, -> (user_id, level) {
+    where(level: level).joins(:users).where(:'users.id' => user_id)
+  }
+
+  validates :title, presence: true 
+  validates :level, numericality: { only_integer: true }
+  validates :title, uniqueness: { scope: :level }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests
   has_many :created_tests, class_name: 'Test', foreign_key: 'author_id'
 
-  def get_tests(level)
-    Test.where(level: level)
-    .joins('JOIN user_tests ON user_tests.test_id = tests.id')
-    .where("user_tests.user_id = ?", id)
-  end
+  validates :name, presence: true
+  validates :email, presence: true
+
+  def tests_by_level(level)
+    Test.by_user_level(id, level)
+  end  
 end
