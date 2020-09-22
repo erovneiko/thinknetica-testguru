cb2b6076ef553786939fd92b525373bf629c288f
Evgeni Rovneiko <erovneiko@mail.ru>
2020-09-22 15:45:11 +0300

    Удаление и обратный вызов

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,14 +1,16 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index show]
+
   def index
-    render html: Test.find(params[:test_id]).questions.pluck(:text)
-                     .map.with_index(1) { |s, i| i.to_s + '. ' + s }.join('\n\n')
-                     .gsub('\n', '<br>').html_safe
+    render html: @test.questions.pluck(:text)
+                      .map.with_index(1) { |s, i| i.to_s + '. ' + s }.join('\n\n')
+                      .gsub('\n', '<br>').html_safe
   end
 
   def show
-    render html: Test.find(params[:test_id]).questions[params[:id].to_i - 1]
-                     .text.insert(0, params[:id] + '. ')
-                     .gsub('\n', '<br>').html_safe
+    render html: @test.questions[params[:id].to_i - 1]
+                      .text.insert(0, params[:id] + '. ')
+                      .gsub('\n', '<br>').html_safe
   end
 
   def create
@@ -16,5 +18,16 @@ class QuestionsController < ApplicationController
     question.answers.build(text: 'dummy')
     question.save
     render plain: "Вопрос успешно создан!"
+  end
+
+  def destroy
+    Question.find(params[:id]).destroy
+    render plain: "Вопрос успешно удалён!"
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
   end
 end
