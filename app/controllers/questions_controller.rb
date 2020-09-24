faeb3d466a9b6b75eeccae0cf5ff7048761264b7
Evgeni Rovneiko <erovneiko@mail.ru>
2020-09-24 20:41:01 +0300

    Исправления

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,7 @@
 class QuestionsController < ApplicationController
-  before_action :find_test
+  before_action :find_test, only: %i[index create]
   before_action :find_question, only: %i[show destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
-  rescue_from ActiveRecord::RecordInvalid, with: :rescue_with_question_invalid
-  rescue_from NoMethodError, with: :rescue_with_question_not_found
 
   def index
     render html: @test.questions.pluck(:text)
@@ -16,15 +14,17 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    render html: @question.text.insert(0, params[:id] + '. ')
-                          .gsub("<pre>", "\n<pre>\n")
-                          .gsub("</pre>", "\n</pre>")
-                          .html_safe
+    render html: @question.text.gsub("<pre>", "\n<pre>\n")
+                               .gsub("</pre>", "\n</pre>")
+                               .html_safe
   end
 
   def create
-    @test.questions.create!(question_params)
-    redirect_to test_questions_path
+    if @test.questions.new(question_params).save
+      redirect_to test_questions_path
+    else
+      redirect_to new_test_question_path(invalid: true)
+    end
   end
 
   def destroy
@@ -34,22 +34,12 @@ class QuestionsController < ApplicationController
 
   private
 
-  # Используется для обоих случаев (не должен вызывать исключение):
-  # /tests/x/questions/y   (y - порядковый номер вопроса в тесте)
-  # /questions/z           (z - идентификатор в базе данных)
   def find_test
-    @test = Test.find(params[:test_id]) unless params[:test_id].nil?
+    @test = Test.find(params[:test_id])
   end
 
-  # Используется для обоих случаев:
-  # /tests/x/questions/y   (y - порядковый номер вопроса в тесте)
-  # /questions/z           (z - идентификатор в базе данных)
   def find_question
-    @question = if !params[:test_id].nil?
-                  @test.questions[params[:id].to_i - 1]
-                else
-                  Question.find(params[:id])
-                end
+    @question = Question.find(params[:id])
   end
 
   def question_params
@@ -58,13 +48,5 @@ class QuestionsController < ApplicationController
 
   def rescue_with_record_not_found(exception)
     render plain: exception.message + "!\n"
-  end
-
-  def rescue_with_question_not_found
-    render plain: "Вопрос не найдён!\n"
-  end
-
-  def rescue_with_question_invalid
-    render plain: "Введённые данные некорректны!\n"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :tests, only: [] do
-    resources :questions, except: %i[edit update]
-    resources :questions, shallow: true, only: %i[show destroy]
+    resources :questions, shallow: true, except: %i[edit update]
   end
 end
