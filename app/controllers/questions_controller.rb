a179457f8189c50e3bc62f8920a5eb0e014e98aa
Evgeni Rovneiko <erovneiko@mail.ru>
2020-09-24 15:27:33 +0300

    Исправления

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,38 +1,64 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index show]
+  before_action :find_test
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :rescue_with_question_invalid
   rescue_from NoMethodError, with: :rescue_with_question_not_found
 
   def index
     render html: @test.questions.pluck(:text)
-                      .map.with_index(1) { |s, i| i.to_s + '. ' + s }.join('\n\n')
-                      .gsub('\n', '<br>').html_safe
+                      .map.with_index(1) { |s, i| i.to_s + '. ' + s }
+                      .join("\n<br><br>\n")
+                      .gsub("<pre>", "\n<pre>\n")
+                      .gsub("</pre>", "\n</pre>")
+                      .gsub("</pre>\n<br><br>", "</pre>")
+                      .html_safe
   end
 
   def show
-    render html: @test.questions[params[:id].to_i - 1]
-                      .text.insert(0, params[:id] + '. ')
-                      .gsub('\n', '<br>').html_safe
+    question = if !params[:test_id].nil?
+                 @test.questions[params[:id].to_i - 1]
+               else
+                 Question.find(params[:id])
+               end
+    render html: question.text.insert(0, params[:id] + '. ')
+                         .gsub("<pre>", "\n<pre>\n")
+                         .gsub("</pre>", "\n</pre>")
+                         .html_safe
   end
 
   def create
-    question = Question.create(text: params[:question][:text], test_id: params[:test_id])
-    question.answers.build(text: 'dummy')
-    question.save
-    render plain: "Вопрос успешно создан!"
+    @test.questions.create!(question_params)
+    redirect_to test_questions_path
   end
 
   def destroy
-    Question.find(params[:id]).destroy
-    render plain: "Вопрос успешно удалён!"
+    if !params[:test_id].nil?
+      @test.questions[params[:id].to_i - 1].destroy
+    else
+      Question.find(params[:id]).destroy
+    end
+    render plain: "Вопрос успешно удалён!\n"
   end
 
   private
 
   def find_test
-    @test = Test.find(params[:test_id])
+    @test = Test.find(params[:test_id]) unless params[:test_id].nil?
+  end
+
+  def question_params
+    params.require(:question).permit(:text)
+  end
+
+  def rescue_with_record_not_found(exception)
+    render plain: exception.message + "!\n"
   end
 
   def rescue_with_question_not_found
-    render plain: 'Вопрос не найдён!'
+    render plain: "Вопрос не найдён!\n"
+  end
+
+  def rescue_with_question_invalid
+    render plain: "Введённые данные некорректны!\n"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tests do
-    resources :questions
+  resources :tests, only: [] do
+    resources :questions, except: %i[edit update]
+    resources :questions, shallow: true, only: %i[show destroy]
   end
 end
