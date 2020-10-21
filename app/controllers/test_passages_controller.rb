2da55c4b7a217c15a58bac97fc516c17f00c7be2
Evgeni Rovneiko <erovneiko@mail.ru>
2020-10-21 10:44:18 +0300

    Замечания

--- app/controllers/test_passages_controller.rb
+++ app/controllers/test_passages_controller.rb
@@ -16,8 +16,7 @@ class TestPassagesController < ApplicationController
     if @test_passage.completed?
       redirect_to result_test_passage_path(@test_passage)
     else
-      show
-      render :show
+      redirect_to @test_passage
     end
   end
 
