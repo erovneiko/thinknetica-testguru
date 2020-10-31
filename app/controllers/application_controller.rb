06eb0520cd30e0ed363babab5830c6ca77a2c489
Evgeni Rovneiko <erovneiko@mail.ru>
2020-10-31 12:49:40 +0300

    Замечания

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def after_sign_in_path_for(resource)
-    current_user.is_a?(Admin) ? admin_tests_path : tests_path
+    resource.admin? ? admin_tests_path : tests_path
   end
 end
--- db/migrate/20201030071433_add_type_attribute_to_users.rb
+++ db/migrate/20201030071433_add_type_attribute_to_users.rb
@@ -1,6 +1,5 @@
 class AddTypeAttributeToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :type, :string, null: false, default: 'User'
-    add_index :users, :type
+    add_column :users, :type, :string, null: false, default: 'User', index: true
   end
 end
