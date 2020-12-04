4bc030c2263203361e9dd96cc0df9193c5a2673f
Evgeni Rovneiko <erovneiko@mail.ru>
2020-12-04 14:16:02 +0300

    Задание 15

---
+++ app/assets/javascripts/password.js
@@ -0,0 +1,27 @@
+document.addEventListener('turbolinks:load', function () {
+  let control = document.getElementById('user_password')
+  if (control) control.addEventListener('input', checkPasswordConfirmation)
+
+  control = document.getElementById('user_password_confirmation')
+  if (control) control.addEventListener('input', checkPasswordConfirmation)
+})
+
+function checkPasswordConfirmation() {
+  let password = document.getElementById('user_password')
+  let confirmation = document.getElementById('user_password_confirmation')
+
+  if (password && confirmation)
+    if (confirmation.value)
+      if (password.value === confirmation.value) {
+        document.querySelector('.octicon-check').classList.remove('hide')
+        document.querySelector('.octicon-x').classList.add('hide')
+      }
+      else {
+        document.querySelector('.octicon-check').classList.add('hide')
+        document.querySelector('.octicon-x').classList.remove('hide')
+      }
+    else {
+      document.querySelector('.octicon-check').classList.add('hide')
+      document.querySelector('.octicon-x').classList.add('hide')
+    }
+}
